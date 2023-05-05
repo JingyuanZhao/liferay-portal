f078e8941494cc2a73705b878cd698c41e991bed
Jessie Zhao <jessie.zhao@liferay.com>
2023-05-05 09:17:41 +0800

    Revert "LPS-182892 It should be false"
    This reverts commit 97af19498e26c90d55a1434a6f5df1ee5cce0969.

--- modules/apps/staging/staging-taglib/src/main/java/com/liferay/staging/taglib/internal/display/context/LayoutsTreeDisplayContext.java
+++ modules/apps/staging/staging-taglib/src/main/java/com/liferay/staging/taglib/internal/display/context/LayoutsTreeDisplayContext.java
@@ -349,7 +349,7 @@ public class LayoutsTreeDisplayContext {
 				"children",
 				layoutsTree.getLayoutsJSONArray(
 					_getSelectedLayoutIdsArray(), _getSelectPagesGroupId(),
-					_httpServletRequest, false, false, false,
+					_httpServletRequest, false, true, false,
 					LayoutConstants.DEFAULT_PARENT_LAYOUT_ID,
 					isSelectPagesPrivateLayout(), _getTreeId())
 			).put(
