548c50a0a34ef81e3ef7cab79748e0d64f51c32c
Jessie Zhao <jessie.zhao@liferay.com>
2023-07-21 13:28:39 +0800

    Revert "LPS-190706 Fix item selector ref"
    This reverts commit 86c8a0aef9d7a8ede8957a25477ade58c3709559.

--- modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/portlet/UsersAdminPortlet.java
+++ modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/portlet/UsersAdminPortlet.java
@@ -61,12 +61,12 @@ public class UsersAdminPortlet extends MVCPortlet {
 			RenderRequest renderRequest, RenderResponse renderResponse)
 		throws IOException, PortletException {
 
-		renderRequest.setAttribute(ItemSelector.class.getName(), itemSelector);
+		renderRequest.setAttribute(ItemSelector.class.getName(), _itemSelector);
 
 		super.render(renderRequest, renderResponse);
 	}
 
 	@Reference
-	protected ItemSelector itemSelector;
+	private ItemSelector _itemSelector;
 
 }
